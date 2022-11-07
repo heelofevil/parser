836d4242d345906b6e955f961e7c108566d49644
Semen <semenspot1@gmail.com>
2022-11-07 17:26:26 +0500

    Upd. buttons and add req

--- prodoc/prodoctorov.py
+++ prodoc/prodoctorov.py
@@ -7,31 +7,30 @@ ua = fake_useragent.UserAgent()
 headers = {"user-agent": ua.random}
 
 """ Start program"""
-city = {1: 'moskva/', 2: 'spb/', 3: 'ekaterinburg/', 4: 'perm/'}
-start_var_1 = city[int(input('Выберите город (1-Москва, 2-Санкт-Петербург, 3-Екатеринбург, 4-Пермь): '))]
+def parsing_prodoc(a,b,c):
 
-# diagnostika и словари узи можно соеденить
-diagnostika = {1: 'uzi_shei', 2: 'uzi_bryushnoy', 3: 'uzi_taza', 4: 'uzi_polovoy'}
-start_var_2 = diagnostika[int(input('Выберите область узи (1-УЗИ шеи, 2-УЗИ брюшной полости, 3-УЗИ малого таза, 4-УЗИ половой системы ): '))]
+    city = {1: 'moskva/', 2: 'spb/', 3: 'ekaterinburg/', 4: 'perm/'}
+    start_var_1 = city[a]
+    diagnostika = {1: 'uzi_shei', 2: 'uzi_bryushnoy', 3: 'uzi_taza', 4: 'uzi_polovoy'}
+    start_var_2 = diagnostika[b]
+    uzi_shei = {1: 'uzi-parashitovidnyh-zhelez/', 2: 'uzi-shitovidnoy-zhelezy/'}
+    uzi_bryushnoy = {1: 'uzi-zheludka/', 2: 'uzi-zhelchnogo-puzyrya/', 3: 'uzi-pecheni/', 4: 'uzi-pochek/', 5: 'uzi-vseh-organov/'}
+    uzi_taza = {1: 'uzi-mochevogo-puzyrya/', 2: 'uzi-mochevogo-puzyrya-s-opredeleniem-ostatochnoy-mochi/'}
+    uzi_polovoy = {1: 'uzi-matki-i-pridatkov/', 2: 'uzi-molochnyh-zhelez/', 3: 'uzi-moshonki/', 4: 'uzi-predstatelnoy-zhelezy/'}
 
-uzi_shei = {1: 'uzi-parashitovidnyh-zhelez/', 2: 'uzi-shitovidnoy-zhelezy/'}
-uzi_bryushnoy = {1: 'uzi-zheludka/', 2: 'uzi-zhelchnogo-puzyrya/', 3: 'uzi-pecheni/', 4: 'uzi-pochek/', 5: 'uzi-vseh-organov/'}
-uzi_taza = {1: 'uzi-mochevogo-puzyrya/', 2: 'uzi-mochevogo-puzyrya-s-opredeleniem-ostatochnoy-mochi/'}
-uzi_polovoy = {1: 'uzi-matki-i-pridatkov/', 2: 'uzi-molochnyh-zhelez/', 3: 'uzi-moshonki/', 4: 'uzi-predstatelnoy-zhelezy/'}
+    vardiagnos = ''
+    if start_var_2 == 'uzi_shei':
+        vardiagnos = uzi_shei[c]
+    elif start_var_2 == 'uzi_bryushnoy':
+        vardiagnos = uzi_bryushnoy[c]
+    elif start_var_2 == 'uzi_taza':
+        vardiagnos = uzi_taza[c]
+    elif start_var_2 == 'uzi_polovoy':
+        vardiagnos = uzi_polovoy[c]
+    """"""
+    url = 'https://prodoctorov.ru/' + start_var_1 + 'diagnostika/' + vardiagnos
 
-vardiagnos = ''
-if start_var_2 == 'uzi_shei':
-    vardiagnos = uzi_shei[int(input("Выберите вид узи (1-паращитовидных желез, 2-щитовидной железы): "))]
-elif start_var_2 == 'uzi_bryushnoy':
-    vardiagnos = uzi_bryushnoy[int(input("Выберите вид узи (1-желудка, 2-желчного пузыря, 3-печени, 4-почек, 5-обзорное всех органов): "))]
-elif start_var_2 == 'uzi_taza':
-    vardiagnos = uzi_taza[int(input("Выберите вид узи (1-мочевого пузыря, 2-УЗИ мочевого пузыря с определением остаточной мочи): "))]
-elif start_var_2 == 'uzi_polovoy':
-    vardiagnos = uzi_polovoy[int(input("Выберите вид узи (1-матки и придатков, 2-молочных желез, 3-мошонки, 4-предстательной железы): "))]
-""""""
-url = 'https://prodoctorov.ru/' + start_var_1 + 'diagnostika/' + vardiagnos
 
-def parsing(url):
     request = requests.get(url, headers=headers)
     pars = BeautifulSoup(request.text, 'lxml')
 
@@ -43,10 +42,5 @@ def parsing(url):
     for i in range(0,len(urls_emerg)):
         total[i] = urls_emerg[i].get_text().strip('\n').strip(' ').rstrip('\n'), name_diagnostic[i], price_diagnostic[i], 'https://prodoctorov.ru' + urls_emerg[i]['href']
 
-    return total.values()
-print(*parsing(url))
+    return total
 
-if input('Следующая страница? y/n = ') == 'y':
-    print(*parsing(url+'?page=2'))
-else:
-    print('Спасибо за использование парсера')
--- prodoc/tg_bot.py
+++ prodoc/tg_bot.py
@@ -1,50 +1,87 @@
 from prodoc.tg_token import token
-import requests
 import telebot
 from telebot import types
+from prodoctorov import parsing_prodoc
+
 bot = telebot.TeleBot(token)
-city = {1: 'moskva/', 2: 'spb/', 3: 'ekaterinburg/', 4: 'perm/'}
+cult = []
+
 
 def telegramm_bot(token):
-
     @bot.message_handler(commands=['start'])
     def start_bot(message):
-        bot.send_message(message.chat.id, "Добро пожаловать! Я бот администратор, я могу найти вам самые хорошие цены на УЗИ! =)")
+        bot.send_message(message.chat.id,
+                         "Добро пожаловать! Я бот администратор, я могу найти вам самые хорошие цены на УЗИ! =)")
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("Москва")
         btn2 = types.KeyboardButton("Санкт-Петербург")
         btn3 = types.KeyboardButton("Екатеринбург")
-        markup.add(btn1, btn2, btn3)
+        btn4 = types.KeyboardButton("Пермь")
+        markup.add(btn1, btn2, btn3, btn4)
         bot.send_message(message.chat.id, 'Выберите город (Москва, Санкт-Петербург, Екатеринбург, Пермь)',
                          reply_markup=markup)
 
-    @bot.message_handler(commands=['button'])
-    def button_message(message):
-        pass
-        #markup.add(types.KeyboardButton("Пермь"))
-        # bot.send_message(message.chat.id, 'Выберите город (1-Москва, 2-Санкт-Петербург, 3-Екатеринбург, 4-Пермь): ', reply_markup=markup)
 
     @bot.message_handler(content_types='text')
     def fed(message):
-        if message.text in "Москва":
+        if message.text in "МоскваСанкт-ПетербургЕкатеринбург":
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1 = types.KeyboardButton("Gorod")
-            markup.add(item1)
-            bot.send_message(message.chat.id, 'asdada: ',reply_markup=markup)
+            item1, item2 = types.KeyboardButton("УЗИ шеи"),types.KeyboardButton("УЗИ брюшной полости")
+            item3, item4 = types.KeyboardButton("УЗИ малого таза"),types.KeyboardButton("УЗИ половой системы")
+            markup.add(item1, item2, item3, item4)
+            bot.send_message(message.chat.id, 'Выбери область узи', reply_markup=markup)
 
-    #
-    # @bot.message_handler(content_types='text')
-    # def select_city(message):
-    #     if message.text == "Москва":
-    #         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    #         item1 = types.KeyboardButton("Gorod")
-    #         markup.add(item1)
-    #         bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
-    #     elif message.text == "Gorod":
-    #         bot.send_message(message.chat.id, 'Спасибо за прочтение статьи!')
+                
+        if message.text in "УЗИ шеи":
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1, item2 = types.KeyboardButton("паращитовидных желез"), types.KeyboardButton("щитовидной железы")
+            markup.add(item1, item2)
+            bot.send_message(message.chat.id, 'Выберите вид узи', reply_markup=markup)
 
 
-    bot.polling()
+        if message.text in "УЗИ брюшной полости":
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1, item2, item3 = types.KeyboardButton("желудка"), types.KeyboardButton("желчного пузыря"), types.KeyboardButton("печени")
+            item4, item5 = types.KeyboardButton("почек"), types.KeyboardButton("обзорное всех органов")
+            markup.add(item1, item2, item3, item4, item5)
+            bot.send_message(message.chat.id, 'Выберите вид узи', reply_markup=markup)
+
+        if message.text in "УЗИ малого таза":
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1, item2 = types.KeyboardButton("мочевого пузыря"), types.KeyboardButton("УЗИ м.п. с определением остаточной мочи")
+            markup.add(item1, item2)
+            bot.send_message(message.chat.id, 'Выберите вид узи', reply_markup=markup)
+
+        if message.text in "УЗИ половой системы":
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1, item2 = types.KeyboardButton("матки и придатков"), types.KeyboardButton("молочных желез")
+            item3, item4 = types.KeyboardButton("мошонки"), types.KeyboardButton("предстательной железы")
+            markup.add(item1, item2,item3, item4)
+            bot.send_message(message.chat.id, 'Выберите вид узи', reply_markup=markup)
+
+
+        if message.text in 'Москва паращитовидных желез желудка мочевого пузыря матки и придатков УЗИ шеи':
+            cult.append(1)
+        elif message.text in 'Санкт-Петербург щитовидной железы желчного пузыря УЗИ м.п. с определением остаточной мочи молочных желез УЗИ брюшной полости':
+            cult.append(2)
+        elif message.text in 'Екатеринбург печени мошонки УЗИ малого таза':
+            cult.append(3)
+        elif message.text in 'Пермь почек предстательной железы УЗИ половой системы':
+            cult.append(4)
+        elif message.text in 'обзорное всех органов':
+            cult.append(5)
+        if len(cult) == 3:
+            status = parsing_prodoc(*cult)
+            for i, k in status.items():
+                bot.send_message(message.chat.id, f'Учреждение: {k[0]} {k[1]} цена {k[2]} \n[Cсылка для записи]({k[3]})', parse_mode='Markdown')
+
+
+
+
+
+
+    bot.infinity_polling()
+
 
 if __name__ == '__main__':
     telegramm_bot(token)
